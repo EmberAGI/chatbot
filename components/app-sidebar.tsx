a1cae89588b1ab1a2b5beeaa5f94aa8e5cef3cff
David Correa <david.correav@gmail.com>
2025-04-16 21:30:30 -0500

    test

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -35,7 +35,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               className="flex flex-row gap-3 items-center"
             >
               <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
-                Chatbot
+                { process.env.PROJECT_NAME || 'EmberAI'}
               </span>
             </Link>
             <Tooltip>
