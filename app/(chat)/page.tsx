58f818d928d13d2f20701ffddf9a3b604348e35e
David Correa <david.correav@gmail.com>
2025-04-18 20:56:22 -0500

    Initial

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -4,8 +4,35 @@ import { Chat } from '@/components/chat';
 import { DEFAULT_CHAT_MODEL } from '@/lib/ai/models';
 import { generateUUID } from '@/lib/utils';
 import { DataStreamHandler } from '@/components/data-stream-handler';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
 
 export default async function Page() {
+
+  const config = getDefaultConfig({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+  });
+  const queryClient = new QueryClient();
+
+
   const id = generateUUID();
   const cookieStore = await cookies();
   const modelIdFromCookie = cookieStore.get('chat-model');
@@ -30,6 +57,9 @@ export default async function Page() {
  
     return (
       <>
+      <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
         <Chat
           key={id}
           id={id}
@@ -39,7 +69,10 @@ export default async function Page() {
           selectedVisibilityType="private"
           isReadonly={false}
         />
-        <DataStreamHandler id={id} />
+              <DataStreamHandler id={id} />
+              </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
       </>
     );
   }
