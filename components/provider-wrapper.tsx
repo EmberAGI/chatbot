0d6ac7b8e65c47717f784cfee1a911c2b72fa1d0
David Correa <david.correav@gmail.com>
2025-04-24 23:29:41 -0500

    test

---
+++ components/provider-wrapper.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  darkTheme,
+  getDefaultConfig,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import {
+  cookieStorage,
+  cookieToInitialState,
+  createStorage,
+  WagmiProvider,
+} from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import React from "react";
+import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth";
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import { Session } from "next-auth";
+
+export function ProviderWrapper({
+  Component,
+  pageProps,
+}: AppProps<{ session: Session }>) {
+  const config = getDefaultConfig({
+    appName: "My RainbowKit App",
+    projectId: "4b49e5e63b9f6253943b470873b47208",
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+    storage: createStorage({ storage: cookieStorage }),
+  });
+
+  const queryClient = new QueryClient();
+  const cookie = cookieStorage.getItem("wagmi.storage") || "";
+  const initialState = cookieToInitialState(config, cookie);
+
+  return (
+    <>
+      <WagmiProvider
+        config={config}
+        reconnectOnMount={true}
+        initialState={initialState}
+      >
+        <SessionProvider refetchInterval={0} session={pageProps.session}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitSiweNextAuthProvider>
+              {" "}
+              <RainbowKitProvider
+                theme={darkTheme({
+                  accentColor: "#FF7224",
+                  accentColorForeground: "#fff",
+                })}
+              >
+                <Component {...pageProps} />
+              </RainbowKitProvider>
+            </RainbowKitSiweNextAuthProvider>
+          </QueryClientProvider>
+        </SessionProvider>
+      </WagmiProvider>
+    </>
+  );
+}
