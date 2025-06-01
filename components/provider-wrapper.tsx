75b58976ff78bdb47eb9efed23f3b917e57ab655
Juan Sebastián Cárdenas Rodríguez <31258166+juanscr@users.noreply.github.com>
2025-05-31 21:41:59 -0500

    Fix reactivity bugs with session.

--- components/provider-wrapper.tsx
+++ components/provider-wrapper.tsx
@@ -14,24 +14,17 @@ import {
 } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import React, { useMemo } from "react";
+import React from "react";
 import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth";
 
-export async function ProviderWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const config =
-    getDefaultConfig({
-      appName: "My RainbowKit App",
-      projectId: "4b49e5e63b9f6253943b470873b47208",
-      chains: [mainnet, polygon, optimism, arbitrum, base],
-      ssr: true, // If your dApp uses server side rendering (SSR)
-      storage: createStorage({ storage: cookieStorage }),
-    });
-   
- 
+export function ProviderWrapper({ children }: { children: React.ReactNode }) {
+  const config = getDefaultConfig({
+    appName: "My RainbowKit App",
+    projectId: "4b49e5e63b9f6253943b470873b47208",
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+    storage: createStorage({ storage: cookieStorage }),
+  });
 
   const queryClient = new QueryClient();
   const cookie = cookieStorage.getItem("wagmi.storage") || "";
